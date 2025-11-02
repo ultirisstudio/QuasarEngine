13ebd7bf5d290bd236c04f6fc6be54865a76b990
Kurogenshi <rouffet.julien@outlook.fr>
2025-11-02 22:41:12 +0100

    Update

---
+++ vendor/sol2/premake5.lua
@@ -0,0 +1,37 @@
+project "sol2"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/**.hpp",
+		"src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"include",
+		
+		"%{IncludeDir.lua}"
+	}
+	
+	links
+	{
+		"lua"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
